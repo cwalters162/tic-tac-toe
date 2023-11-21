9c471a950eff69040e2ad6593dda20c8985c91cf
Cherokee Walters <cherokeewalters162@gmail.com>
2023-11-21 00:29:02 -0500

    Added some styling and a basic win condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState} from "react";
 
 enum TileType {
   X = "X",
@@ -6,7 +6,7 @@ enum TileType {
   Empty = "",
 }
 
-const initialGameboardState = [
+const initialGameBoardState = [
     [TileType.Empty, TileType.Empty, TileType.Empty],
     [TileType.Empty, TileType.Empty, TileType.Empty],
     [TileType.Empty, TileType.Empty, TileType.Empty],
@@ -17,10 +17,25 @@ enum Player {
   O = "O",
 }
 
+enum GameState {
+  PLAYING,
+  DRAW,
+  WIN,
+  LOSS,
+}
+
 function App() {
-  const [gameBoard, setGameBoard] = useState(initialGameboardState);
+  const [gameBoard, setGameBoard] = useState(initialGameBoardState);
   const [turn, setTurn] = useState<Player>(Player.X)
+  const [gameState, setGameState] = useState<GameState>(GameState.PLAYING)
   const [prompt, setPrompt] = useState("")
+
+  function checkWin() {
+    if (gameBoard[0][0] == TileType.X && gameBoard[0][1] == TileType.X && gameBoard[0][2] == TileType.X) {
+      setGameState(GameState.WIN)
+      setPrompt(`Player fillme won!`)
+    }
+  }
 
   function claimedTile() {
     setPrompt("Please click an unclaimed tile")
@@ -32,6 +47,7 @@ function App() {
         const newGameBoard = gameBoard
         newGameBoard[x][y] = TileType.X
         setGameBoard(newGameBoard);
+        checkWin()
         setTurn(Player.O)
         setPrompt("")
         break;
@@ -40,6 +56,7 @@ function App() {
         const newGameBoard = gameBoard
         newGameBoard[x][y] = TileType.O
         setGameBoard(newGameBoard);
+        checkWin()
         setTurn(Player.X)
         setPrompt("")
         break;
@@ -64,25 +81,35 @@ function App() {
     }
   }
 
+  if (gameState == GameState.WIN) {
+    return (
+        <div className={"flex flex-col justify-center items-center w-screen h-screen bg-black text-custom-text"}>
+          <span>Congratulations you won!</span>
+          <span>Play again?</span>
+          <button className={"border-2 border"}>Yes!</button>
+        </div>
+    )
+  }
+
   return (
-      <div className={"flex flex-col justify-center items-center w-screen h-screen bg-black"}>
-        <span className={"text-white text-5xl pb-5"}>Current Player: {turn}</span>
-        <div className={"flex justify-center"}>
+      <div className={"flex flex-col justify-center items-center w-screen h-screen bg-custom-bg"}>
+        <span className={"text-5xl pb-5 text-custom-text"}>Current Player: {turn}</span>
+        <div className={"flex justify-center border-y-2 border-custom-border"}>
           <Tile display={gameBoard[0][0]} handleOnClick={() => handleOnClick(0, 0)}/>
           <Tile display={gameBoard[0][1]} handleOnClick={() => handleOnClick(0, 1)}/>
           <Tile display={gameBoard[0][2]} handleOnClick={() => handleOnClick(0, 2)}/>
         </div>
-        <div className={"flex justify-center"}>
+        <div className={"flex justify-center border-b-2 border-custom-border"}>
           <Tile display={gameBoard[1][0]} handleOnClick={() => handleOnClick(1, 0)}/>
           <Tile display={gameBoard[1][1]} handleOnClick={() => handleOnClick(1, 1)}/>
           <Tile display={gameBoard[1][2]} handleOnClick={() => handleOnClick(1, 2)}/>
         </div>
-        <div className={"flex justify-center"}>
+        <div className={"flex justify-center border-b-2 border-custom-border"}>
           <Tile display={gameBoard[2][0]} handleOnClick={() => handleOnClick(2, 0)}/>
           <Tile display={gameBoard[2][1]} handleOnClick={() => handleOnClick(2, 1)}/>
           <Tile display={gameBoard[2][2]} handleOnClick={() => handleOnClick(2, 2)}/>
         </div>
-        <span className={"text-white pt-5 text-2xl min-h-[4rem]"}>{prompt}</span>
+        <span className={"pt-5 text-2xl min-h-[4rem] text-custom-text"}>{prompt}</span>
       </div>
   )
 }
@@ -94,7 +121,7 @@ interface TileProps {
 function Tile({display, handleOnClick}: TileProps) {
   return (
       <div
-          className={"min-w-[8rem] min-h-[9rem] text-9xl flex justify-center items-center bg-white border-yellow-400 border-2"}
+          className={"min-w-[9rem] min-h-[9rem] text-9xl flex justify-center items-center bg-custom-primary border-custom-border first:border-l-2 border-r-2 hover:bg-custom-highlight text-custom-text"}
           onClick={() => handleOnClick()}>
         {display}
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+          {
+            "custom-bg": "#0f0606",
+            "custom-highlight": "#490000",
+            "custom-primary": "#2f0000",
+            "custom-border": "#650000",
+              "custom-text": "#DDDDDD",
+          }
+    },
   },
   plugins: [],
 }
