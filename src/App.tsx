cc3357302ef9a6894c6c89e695662a536375155d
Cherokee Walters <cherokeewalters162@gmail.com>
2023-11-20 23:24:02 -0500

    Game works need ending condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import { useState } from "react";
 
 enum TileType {
   X = "X",
@@ -13,79 +13,91 @@ const initialGameboardState = [
 ]
 
 enum Player {
-  X,
-  O,
-}
-
-function setTile(turn: Player) {
-  switch (turn) {
-    case Player.X: return TileType.X;
-    case Player.O: return TileType.O;
-  }
+  X = "X",
+  O = "O",
 }
 
 function App() {
-  const [gameboard, setGameboard] = useState(initialGameboardState);
+  const [gameBoard, setGameBoard] = useState(initialGameboardState);
   const [turn, setTurn] = useState<Player>(Player.X)
+  const [prompt, setPrompt] = useState("")
 
+  function claimedTile() {
+    setPrompt("Please click an unclaimed tile")
+  }
 
-  function handleOnClick(event: React.MouseEvent<HTMLDivElement>) {
-    event.preventDefault();
-    const target = event.target as HTMLDivElement;
-    const id = target.id
-    const location = id.split("-");
-    const x = parseInt(location[0]);
-    const y = parseInt(location[1]);
-    console.log(gameboard[x][y])
-    console.log(TileType.Empty)
-
-    if (gameboard[x][y] == TileType.Empty) {
-      setGameboard((prevState) => {
-        const new_state = prevState
-        new_state[x][y] = setTile(turn);
-        return new_state
-      })
+  function claimTile(turn: Player, x: number, y: number) {
+    switch (turn) {
+      case Player.X: {
+        const newGameBoard = gameBoard
+        newGameBoard[x][y] = TileType.X
+        setGameBoard(newGameBoard);
+        setTurn(Player.O)
+        setPrompt("")
+        break;
+      }
+      case Player.O: {
+        const newGameBoard = gameBoard
+        newGameBoard[x][y] = TileType.O
+        setGameBoard(newGameBoard);
+        setTurn(Player.X)
+        setPrompt("")
+        break;
+      }
     }
-    console.log(`Clicked on: ${id}`)
-    console.log(`Location at: X:${x} Y: ${y}`)
+  }
+
+  function handleOnClick(x: number, y: number) {
+    switch (gameBoard[x][y]) {
+      case TileType.Empty: {
+        claimTile(turn, x, y)
+        break;
+      }
+      case TileType.X: {
+        claimedTile()
+        break;
+      }
+      case TileType.O: {
+        claimedTile()
+        break;
+      }
+    }
   }
 
   return (
       <div className={"flex flex-col justify-center items-center w-screen h-screen bg-black"}>
-        {gameboard.map((row, index) => {
-          let rowId = "";
-
-          switch (index) {
-            case 0: rowId = "top-row"; break;
-            case 1: rowId = "mid-row"; break;
-            case 2: rowId = "bottom-row"; break;
-          }
-
-          return (
-              <div id={rowId} key={rowId} className={"flex justify-center"}>
-                {row.map((tile, y) => {
-                  let prefix = "";
-                  let divId = ""
-                  switch (index) {
-                    case 0: prefix = "0-"; break;
-                    case 1: prefix = "1-"; break;
-                    case 2: prefix = "2-"; break;
-                  }
-
-                  switch (y) {
-                    case 0: divId = prefix + "0"; break;
-                    case 1: divId = prefix + "1"; break;
-                    case 2: divId = prefix + "2"; break;
-                  }
-
-                  return (<div id={divId} key={divId} className={"min-w-[8rem] min-h-[8rem] text-9xl flex justify-center items-center bg-white border-yellow-400 border-2"} onClick={handleOnClick}>{tile}</div>)
-                })
-                }
-              </div>
-          )
-        })}
+        <span className={"text-white text-5xl pb-5"}>Current Player: {turn}</span>
+        <div className={"flex justify-center"}>
+          <Tile display={gameBoard[0][0]} handleOnClick={() => handleOnClick(0, 0)}/>
+          <Tile display={gameBoard[0][1]} handleOnClick={() => handleOnClick(0, 1)}/>
+          <Tile display={gameBoard[0][2]} handleOnClick={() => handleOnClick(0, 2)}/>
+        </div>
+        <div className={"flex justify-center"}>
+          <Tile display={gameBoard[1][0]} handleOnClick={() => handleOnClick(1, 0)}/>
+          <Tile display={gameBoard[1][1]} handleOnClick={() => handleOnClick(1, 1)}/>
+          <Tile display={gameBoard[1][2]} handleOnClick={() => handleOnClick(1, 2)}/>
+        </div>
+        <div className={"flex justify-center"}>
+          <Tile display={gameBoard[2][0]} handleOnClick={() => handleOnClick(2, 0)}/>
+          <Tile display={gameBoard[2][1]} handleOnClick={() => handleOnClick(2, 1)}/>
+          <Tile display={gameBoard[2][2]} handleOnClick={() => handleOnClick(2, 2)}/>
+        </div>
+        <span className={"text-white pt-5 text-2xl min-h-[4rem]"}>{prompt}</span>
       </div>
+  )
+}
+interface TileProps {
+  display: string,
+  handleOnClick: () => void
+}
 
+function Tile({display, handleOnClick}: TileProps) {
+  return (
+      <div
+          className={"min-w-[8rem] min-h-[9rem] text-9xl flex justify-center items-center bg-white border-yellow-400 border-2"}
+          onClick={() => handleOnClick()}>
+        {display}
+      </div>
   )
 }
 
