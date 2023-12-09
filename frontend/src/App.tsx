a6e9837b74237cf4e918fee7b8e6e5f87faa9883
Cherokee Walters <cherokeewalters162@gmail.com>
2023-12-09 00:18:13 -0500

    Pulled state into a reducer for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,123 +1,44 @@
-import { useState} from "react";
-
-enum TileType {
-  X = "X",
-  O = "O",
-  Empty = "",
-}
+import {useReducer} from "react";
+import Tile from "./components/Tile.tsx";
+import {AppState, Command, GameStatus, Player, reducer, TileType} from "./reducers/appReducer.ts";
 
 const initialGameBoardState = [
-    [TileType.Empty, TileType.Empty, TileType.Empty],
-    [TileType.Empty, TileType.Empty, TileType.Empty],
-    [TileType.Empty, TileType.Empty, TileType.Empty],
+  [TileType.Empty, TileType.Empty, TileType.Empty],
+  [TileType.Empty, TileType.Empty, TileType.Empty],
+  [TileType.Empty, TileType.Empty, TileType.Empty],
 ]
 
-enum Player {
-  X = "X",
-  O = "O",
-}
-
-enum GameState {
-  PLAYING,
-  DRAW,
-  WIN,
+const initialState: AppState = {
+  gameBoard: initialGameBoardState,
+  gameStatus: GameStatus.PLAYING,
+  prompt: "",
+  turn: Player.X
 }
 
 function App() {
-  const [gameBoard, setGameBoard] = useState(initialGameBoardState);
-  const [gameState, setGameState] = useState<GameState>(GameState.PLAYING)
-  const [turn, setTurn] = useState<Player>(Player.X)
-  const [prompt, setPrompt] = useState("")
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { prompt, turn, gameStatus, gameBoard } = state;
 
-  function checkForThreeInARow(tileType: TileType) {
-    for (let i = 0; i < 3; i++) {
-      if (gameBoard[i][0] === tileType && gameBoard[i][1] === tileType && gameBoard[i][2] === tileType) {
-        return true
-      }
-      if (gameBoard[0][i] === tileType && gameBoard[1][i] === tileType && gameBoard[2][i] === tileType) {
-        return true
-      }
-    }
-
-    if (gameBoard[0][0] === tileType && gameBoard[1][1] === tileType && gameBoard[2][2] === tileType) {
-      return true
-    }
-
-    return gameBoard[0][2] === tileType && gameBoard[1][1] === tileType && gameBoard[2][0] === tileType;
-  }
-
-  function checkWin() {
-    const emptyTiles = gameBoard.flatMap((row) => {
-      return row.filter((tile)=> {
-        return tile == TileType.Empty;
-      })
-    })
-    if (checkForThreeInARow(TileType.X)) {
-      setGameState(GameState.WIN)
-      setPrompt(`Player X won!`)
-    } else if (checkForThreeInARow(TileType.O)) {
-      setGameState(GameState.WIN)
-      setPrompt(`Player O won!`)
-    } else if (emptyTiles.length === 0) {
-      setGameState(GameState.DRAW);
-      setPrompt("Draw!")
-    }
-  }
-
-  function claimedTile() {
-    setPrompt("Please click an unclaimed tile")
-  }
-
-  function claimTile(turn: Player, x: number, y: number) {
-    setPrompt("")
-    switch (turn) {
-      case Player.X: {
-        const newGameBoard = gameBoard
-        newGameBoard[x][y] = TileType.X
-        setGameBoard(newGameBoard);
-        checkWin()
-        setTurn(Player.O)
-        break;
-      }
-      case Player.O: {
-        const newGameBoard = gameBoard
-        newGameBoard[x][y] = TileType.O
-        setGameBoard(newGameBoard);
-        checkWin()
-        setTurn(Player.X)
-        break;
-      }
-    }
-  }
-
-  function resetGame() {
-    const board = [
-      [TileType.Empty, TileType.Empty, TileType.Empty],
-      [TileType.Empty, TileType.Empty, TileType.Empty],
-      [TileType.Empty, TileType.Empty, TileType.Empty],
-    ]
-    setPrompt("");
-    setGameState(GameState.PLAYING);
-    setGameBoard(board);
-    setTurn(Player.X);
+  function handleResetOnClick() {
+    dispatch({command: Command.Reset, payload: null})
   }
 
   function handleOnClick(x: number, y: number) {
-    if (gameState != GameState.PLAYING) {
+    if (gameStatus != GameStatus.PLAYING) {
       return
     }
 
     switch (gameBoard[x][y]) {
       case TileType.Empty: {
-        claimTile(turn, x, y)
+        dispatch({command: Command.ClaimTile, payload: {x: x, y: y} })
         break;
       }
       case TileType.X: {
-        claimedTile()
+        dispatch({command: Command.ClaimedTile, payload: null })
         break;
       }
       case TileType.O: {
-        claimedTile()
+        dispatch({command: Command.ClaimedTile, payload: null })
         break;
       }
     }
@@ -142,25 +63,11 @@ function App() {
           <Tile display={gameBoard[2][2]} handleOnClick={() => handleOnClick(2, 2)}/>
         </div>
         <span className={"pt-5 text-2xl min-h-[4rem] text-custom-text"}>{prompt}</span>
-        {!(gameState === GameState.PLAYING) ?
-            <button className={"border-custom-border border-2 text-custom-text px-2 text-2xl bg-custom-primary hover:bg-custom-highlight rounded-lg"} onClick={resetGame}>Play Again?</button>
+        {!(gameStatus === GameStatus.PLAYING) ?
+            <button className={"border-custom-border border-2 text-custom-text px-2 text-2xl bg-custom-primary hover:bg-custom-highlight rounded-lg"} onClick={handleResetOnClick}>Play Again?</button>
             :
             <div className={"min-h-[2.3rem]"}/>
         }
-      </div>
-  )
-}
-interface TileProps {
-  display: string,
-  handleOnClick: () => void
-}
-
-function Tile({display, handleOnClick}: TileProps) {
-  return (
-      <div
-          className={" min-h-[5rem] min-w-[5rem] sm:min-w-[9rem] sm:min-h-[9rem] text-5xl sm:text-9xl flex justify-center items-center bg-custom-primary border-custom-border first:border-l-2 border-r-2 hover:bg-custom-highlight text-custom-text"}
-          onClick={() => handleOnClick()}>
-        {display}
       </div>
   )
 }
---
+++ frontend/src/components/Tile.tsx
@@ -0,0 +1,14 @@
+interface TileProps {
+    display: string,
+    handleOnClick: () => void
+}
+
+export default function Tile({display, handleOnClick}: TileProps) {
+    return (
+        <div
+            className={" min-h-[5rem] min-w-[5rem] sm:min-w-[9rem] sm:min-h-[9rem] text-5xl sm:text-9xl flex justify-center items-center bg-custom-primary border-custom-border first:border-l-2 border-r-2 hover:bg-custom-highlight text-custom-text"}
+            onClick={() => handleOnClick()}>
+            {display}
+        </div>
+    )
+}
